cfe16ef407ccf5a4c30ff721a34444b1d77ae593
赵杰 <zhaojay1211@gmail.com>
2025-06-29 20:59:20 +0800

    feat: add dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,24 @@
-const Page = () => {
-  return <div>(platform)</div>;
+import { redirect } from "next/navigation";
+import { verifyJwt } from "@/lib/auth/jwt";
+import { cookies } from "next/headers";
+
+const Page = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const payload = token ? await verifyJwt(token) : null;
+
+  if (!payload) {
+    redirect("/login");
+  }
+
+  if (payload.role === "admin") {
+    redirect("/admin");
+  } else if (payload.role === "merchant") {
+    redirect("/merchant");
+  } else {
+    redirect("/login");
+  }
 };
 
 export default Page;
